2f4e552b400533d7fd0ff982224fc708dcc8cc01
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2026-03-09 20:15:52 -0300

    f

--- Back/Migrations/20260306173134_AgregarFechasArmado.cs
+++ Back/Migrations/20260306173134_AgregarFechasArmado.cs
@@ -10,13 +10,29 @@ namespace Back.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaInicioArmado",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: true);
 
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaFinArmado",
+                table: "Pedidos",
+                type: "datetime2",
+                nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropColumn(
+                name: "FechaInicioArmado",
+                table: "Pedidos");
 
+            migrationBuilder.DropColumn(
+                name: "FechaFinArmado",
+                table: "Pedidos");
         }
     }
 }
