114aa1da25bd6815de3979803c27266abb9d7619
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2026-03-18 20:27:21 -0300

    nuevamigracion

---
+++ Back/Migrations/20260318232536_CaptureCurrentModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Back.Migrations
+{
+    /// <inheritdoc />
+    public partial class CaptureCurrentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "IntentosEntregaFallida",
+                table: "HistorialesDeEstados",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IntentosMax",
+                table: "HistorialesDeEstados",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IntentosEntregaFallida",
+                table: "HistorialesDeEstados");
+
+            migrationBuilder.DropColumn(
+                name: "IntentosMax",
+                table: "HistorialesDeEstados");
+        }
+    }
+}
