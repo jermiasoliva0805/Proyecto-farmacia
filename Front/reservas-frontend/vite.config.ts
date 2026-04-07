83b8c9780d4213bfa1d6d9fcc79541c694c780c2
AgusAllende457 <agustina.allende.mansilla@mi.unc.edu.ar>
2026-04-07 20:28:12 -0300

    cambios en vite.config.ts

--- Front/reservas-frontend/vite.config.ts
+++ Front/reservas-frontend/vite.config.ts
@@ -1,29 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            '@components': path.resolve(__dirname, './src/components'),
-            '@pages': path.resolve(__dirname, './src/pages'),
-            '@services': path.resolve(__dirname, './src/service'),
-            '@types': path.resolve(__dirname, './src/types'),
-            '@utils': path.resolve(__dirname, './src/utils'),
-            '@context': path.resolve(__dirname, './src/context'),
-        },
+plugins: [react()],
+resolve: {
+    alias: {
+    '@': path.resolve(__dirname, './src'),
+    '@components': path.resolve(__dirname, './src/components'),
+    '@pages': path.resolve(__dirname, './src/pages'),
+    '@services': path.resolve(__dirname, './src/service'),
+    '@types': path.resolve(__dirname, './src/types'),
+    '@utils': path.resolve(__dirname, './src/utils'),
+    '@context': path.resolve(__dirname, './src/context'),
     },
-    server: {
-        port: 5173,
-        open: true,
-        middlewareMode: false,
-    },
-    publicDir: 'public',
-    build: {
-        outDir: 'dist',
-        sourcemap: false,
-        minify: 'terser',
-    }
+},
+server: {
+    port: 5173,
+    open: true,
+    middlewareMode: false,
+},
+publicDir: 'public',
+build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+},
 })
