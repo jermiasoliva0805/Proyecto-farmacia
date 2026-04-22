f54bfe8b742c101edd9b8780bf8d32266c776fba
Flor32 <flormeda27@gmail.com>
2026-04-22 16:56:45 -0300

    Update vite-env.d.ts

--- Front/reservas-frontend/src/vite-env.d.ts
+++ Front/reservas-frontend/src/vite-env.d.ts
@@ -2,6 +2,8 @@ interface ImportMetaEnv {
     readonly VITE_API_BASE_URL: string;
     readonly VITE_APP_NAME: string;
     readonly VITE_USE_COOKIES: string;
+    readonly VITE_ORDER_PRINT_PATH?: string;
+    readonly VITE_ORDER_LABEL_PATH?: string;
 }
 
 interface ImportMeta {
