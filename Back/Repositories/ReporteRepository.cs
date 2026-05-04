7fe6c72833496501ed15a6c89762f6a2545af85e
AgusAllende457 <agustina.allende.mansilla@mi.unc.edu.ar>
2026-05-04 18:29:17 -0300

    ...

--- Back/Repositories/ReporteRepository.cs
+++ Back/Repositories/ReporteRepository.cs
@@ -558,7 +558,7 @@ namespace Back.Repositories
  
             var pedidosFueraDeplazo = pedidosEntregados
                 .Where(p => p.FechaEntregaReal.HasValue &&
-                            p.FechaEntregaReal.Value.Date > p.FechaEntregaEstimada.Date)
+                            p.FechaEntregaReal.Value > p.FechaEntregaEstimada)
                 .ToList();
  
             var detalles = pedidosFueraDeplazo
